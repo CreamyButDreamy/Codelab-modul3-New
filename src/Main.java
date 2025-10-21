edb9b9161cfa05cfb9e7b1d3436b276fb0ff6455
CreamyButDreamy <syakhishhani@gmail.com>
2025-10-21 08:17:01 +0700

    Initial commit: Menambahkan proyek Kalkulator Sederhana

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int angka1 = 10;
+        int angka2 = 5;
+
+        int hasilTambah = angka1 + angka2;
+        System.out.println("Hasil Penjumlahan: " + hasilTambah);
+    }
+}
